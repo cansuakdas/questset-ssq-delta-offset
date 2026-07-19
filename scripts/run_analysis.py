e24c0f0ab1c2cd695f77f0dc9f5ad9cac6ee3092
Cansu Akdaş <cansuakdas@unknown3274f14734b2.home>
2026-07-19 19:45:09 +0300

    Simplify repository structure

--- scripts/run_analysis.py
+++
@@ -1,4 +0,0 @@
-from questset_analysis.cli import main
-
-if __name__ == "__main__":
-    main()
--- src/questset_analysis/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import AnalysisConfig, analyze_session, compute_ssq_scores
-
-__all__ = ["AnalysisConfig", "analyze_session", "compute_ssq_scores"]
